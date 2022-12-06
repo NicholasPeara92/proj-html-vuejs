c8669768a282394248e759b3300d02e35981372c
NicholasPeara92 <nicholas.peara@gmail.com>
2022-12-06 17:31:09 +0100

    Aggiunge sezione choice

--- src/components/AppNews.vue
+++ src/components/AppNews.vue
@@ -43,6 +43,26 @@ export default {
     <div id="btn">
       <DarkButton />
     </div>
+    <div id="start-choice" class="d-flex">
+      <div class="card-left mx-3">
+        <small>Are You Ready</small>
+        <h4>Start a New project</h4>
+        <form action="submit">
+          <input
+            type="email"
+            class="l"
+            id="exampleFormControlInput1"
+            placeholder="Enter Your Email Adress"
+          />
+          <button id="submit" type="submit">SUBMIT</button>
+        </form>
+      </div>
+      <div class="card-right mx-3">
+        <small>What Are You Waiting For?</small>
+        <h4>Let's Talk About Work</h4>
+        <DarkButton />
+      </div>
+    </div>
   </section>
 </template>
 
@@ -70,5 +90,56 @@ mark {
   text-align: center;
   padding-top: 30px;
   padding-bottom: 200px;
+}
+#start-choice {
+  justify-content: space-between;
+  padding-bottom: 100px;
+  .card-left {
+    background: linear-gradient(270deg, black, rgb(48, 48, 48));
+    width: 50%;
+    border-radius: 20px;
+    box-shadow: 5px 5px 20px rgb(168 168 168);
+  }
+  .card-right {
+    width: 50%;
+    background: linear-gradient(
+      270deg,
+      #0edaa0ff,
+      #30dc90ff,
+      #51dd81ff,
+      #73df71ff,
+      #94e061ff
+    );
+    box-shadow: 5px 5px 15px #baf5e7;
+    border-radius: 20px;
+  }
+}
+.card-left,
+.card-right {
+  color: white;
+  text-align: center;
+  padding: 50px 0;
+  h4 {
+    margin-top: 20px;
+    margin-bottom: 50px;
+    font-weight: bold;
+  }
+  small {
+    color: lightgray;
+  }
+}
+input,
+#submit {
+  color: grey;
+  background-color: var(--primary-color);
+  border: none;
+  padding: 10px 20px;
+  font-size: 14px;
+}
+input {
+  border-radius: 20px 0 0 20px;
+}
+#submit {
+  border-radius: 0 20px 20px 0;
 }
 </style>
